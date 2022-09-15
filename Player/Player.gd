183438368ce39ca3ac625e1066bf40d9f31b52f0
fredriktegnell <Fredrik.Tegnell@hotmail.com>
2022-09-15 11:46:17 +0200

    Basic player actions + basic AI

---
+++ Player/Player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+class_name Player
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+const MAX_SPEED = 100
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector * MAX_SPEED
+	else:
+		velocity = Vector2.ZERO
+			
+	move_and_collide(velocity * delta)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
