02ed701465e24a7c3bf3005604377559bc463b9c
Anthe593 <antonhellstrom2@hotmail.com>
2022-09-29 00:05:28 +0200

    Played with removal of objects and tilemap-manipulation

--- Items/Object/Lever/Lever.gd
+++ Items/Object/Lever/Lever.gd
@@ -1,8 +1,17 @@
 extends Area2D
 
 onready var _animated_sprite = $AnimatedSprite
+onready var _bridge = get_node("../../LevelNavigation/Bridge")
+onready var _bridge_nodes = get_node("../../LevelNavigation/Bridge").get_used_cells()
+onready var _bridge2 = get_node("../../LevelNavigation/Bridge2")
+onready var _bridge_nodes2 = get_node("../../LevelNavigation/Bridge2").get_used_cells()
+onready var _water_collision = get_node("../../LevelNavigator/WaterCollision")
+onready var _ground_path = "../../LevelNavigator/BaseBackground"
+
 var inside = false
+
 export(int) var damage: int = 0
+
 
 
 func _on_Lever_area_entered(area: Area2D)->bool:
@@ -15,14 +24,25 @@ func _on_Lever_area_exited(area:Area2D)->bool:
 	return inside
 
 
-func _input(event):
+func _physics_process(_delta: float)->void:
 	
 	if inside == true:
-		if event is InputEventKey and event.pressed:
-			if event.scancode == KEY_SPACE and _animated_sprite.frame == 0:
+		if Input.is_action_just_pressed("ui_select"):
+			if _animated_sprite.frame == 0:
+				for pos in _bridge_nodes:
+					get_node("../../LevelNavigation/WaterCollision").set_cell(pos[0],pos[1],5)
+				for pos in _bridge_nodes2:
+					get_node("../../LevelNavigation/WaterCollision").set_cell(pos[0],pos[1],-1)
+				get_node("../../LevelNavigation").remove_child(_bridge)
+				get_node("../../LevelNavigation").add_child(_bridge2)
 				_animated_sprite.frame = 1
-
-			elif  event.scancode == KEY_SPACE and _animated_sprite.frame == 1:
+			elif _animated_sprite.frame == 1:
+				for pos in _bridge_nodes:
+					get_node("../../LevelNavigation/WaterCollision").set_cell(pos[0],pos[1],-1)
+				for pos in _bridge_nodes2:
+					get_node("../../LevelNavigation/WaterCollision").set_cell(pos[0],pos[1],5)
+				get_node("../../LevelNavigation").add_child(_bridge)
+				get_node("../../LevelNavigation").remove_child(_bridge2)
 				_animated_sprite.frame = 0
 				
 				
